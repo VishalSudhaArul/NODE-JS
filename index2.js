3580f651077a5caf10cc71d04616ea5eca694e6b
VishalSudhaArul <152048843+VishalSudhaArul@users.noreply.github.com>
2026-02-19 11:33:53 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,13 @@
+import express from "express"
+const app = express() 
+app.listen(8080, () => {
+    console.log("Server started");
+});
+app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("response from server for /")
+})
+app.get("/home",(req,res)=>{
+    console.log(req.url)
+    res.send("response from server for home ")
+})
